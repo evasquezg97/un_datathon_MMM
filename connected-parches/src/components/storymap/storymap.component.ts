d699dd403e9da8d985c889140a41b7d4ca0338d6
pansamaster <juanjoserojasvelilla@gmail.com>
2024-11-10 11:20:08 -0500

    Gato verdolaga

--- connected-parches/src/components/storymap/storymap.component.ts
+++ connected-parches/src/components/storymap/storymap.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { ArrowButtonComponent } from '../arrow-button/arrow-button.component';
+import { MatTabsModule } from '@angular/material/tabs'; // Import MatTabsModule
+import { CommonModule } from '@angular/common'; // Import CommonModule
 
 @Component({
   selector: 'app-storymap',
   standalone: true,
-  imports: [ArrowButtonComponent],
+  imports: [ArrowButtonComponent,MatTabsModule, CommonModule],
   templateUrl: './storymap.component.html',
   styleUrls: ['./storymap.component.css']
 })
