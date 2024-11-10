224e3d1afaaf3fc4fdc282c9e5e69142fd3aaaf2
evasquezg97 <74801856+evasquezg97@users.noreply.github.com>
2024-11-09 20:55:09 -0500

    CP: input and list

--- connected-parches/src/components/application/application.component.ts
+++ connected-parches/src/components/application/application.component.ts
@@ -1,12 +1,30 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatListModule } from '@angular/material/list';
 
 @Component({
   selector: 'app-application',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule, MatInputModule, MatButtonModule, MatListModule],
   templateUrl: './application.component.html',
-  styleUrl: './application.component.css'
+  styleUrls: ['./application.component.css']
 })
 export class ApplicationComponent {
+  inputValue: string = '';
+  items: string[] = []
 
+  addItem() {
+    this.items.push(this.queryItems());
+  }
+
+  queryItems() {
+    if (this.inputValue.trim()) {
+      return this.inputValue
+    }
+
+    return '';
+  }
 }
