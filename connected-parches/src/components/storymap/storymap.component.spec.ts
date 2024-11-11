40d3d9bc38e45587efd65e7c1760341b48c677a0
pansamaster <juanjoserojasvelilla@gmail.com>
2024-11-11 08:41:05 -0500

    styles improved

--- connected-parches/src/components/storymap/storymap.component.spec.ts
+++ connected-parches/src/components/storymap/storymap.component.spec.ts
@@ -1,6 +1,9 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { ArrowButtonComponent } from '../arrow-button/arrow-button.component';
 import { StorymapComponent } from './storymap.component';
+import { MatTabsModule } from '@angular/material/tabs';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CommonModule } from '@angular/common';
 
 describe('StorymapComponent', () => {
   let component: StorymapComponent;
@@ -8,10 +11,17 @@ describe('StorymapComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [StorymapComponent]
+      imports: [
+        StorymapComponent,
+        ArrowButtonComponent,
+        MatTabsModule,
+        BrowserAnimationsModule,
+        CommonModule,
+        
+      ]
     })
     .compileComponents();
-    
+
     fixture = TestBed.createComponent(StorymapComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
